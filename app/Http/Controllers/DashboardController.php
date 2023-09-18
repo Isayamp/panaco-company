cbfa30081aaa4f2f94b326ea654085e680fe49b8
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-18 19:24:28 +0200

    implementation du DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+        /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Afficher le dashboard
+        return view('dashboard', compact('nbreProduits', 'nbreCategories'));
+    }
 }
