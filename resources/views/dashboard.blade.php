1d5bb010e6f85cacd0f0941ae02f0a18188c2503
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-21 22:56:53 +0200

    fix layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 
@@ -148,9 +148,9 @@
 						<div class="card">
 							<div class="card-body">
 								<h5 class="card-title">Produits</h5>
-								<h1 class="mt-1 mb-3">{{ $nbreProduits }}</h1>
+								<h1 class="mt-1 mb-3"> 1 253</h1>
 								<div class="mb-0">
-									<span class="badge badge-primary-light"><i class="mdi mdi-arrow-bottom-right"></i> {{ $nbreCategories }}</span>
+									<span class="badge badge-primary-light"><i class="mdi mdi-arrow-bottom-right"></i> 47</span>
 									<span class="text-muted">Catégories</span>
 								</div>
 							</div>
