bbb6faae941c6138ecad5152c0929f69554810af
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-22 05:43:11 +0200

    implémentation de function store

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produit;
 use App\Models\Categorie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -40,7 +41,32 @@ class ProduitController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validation
+        $request->validate([
+            'designation_produit' => 'required|unique:produits,designation_produit|max:50',
+            'description_produit' => 'required',
+            'prix' => 'required',
+            'categorie_id' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        // Traitement ou récuperation informations de m' image
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $todayDate = date('YmdHist');
+            $imagePath = $request->file('image')->storeAs(
+                'images',
+                $todayDate . '.' . $request->file('image')->getClientOriginalExtension(),
+                'public'
+            );
+        }
+
+        // Enregistrement
+        $produit = Produit::create($request->all());
+
+        $produit->update(['image' => $imagePath]); 
+
+        return redirect()->route('produits.index')->with('success', 'Produit ajouté avec succès.');
     }
 
     /**
