3d9f07b3576ff46d57a9965a57c23ab407846251
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-22 05:32:08 +0200

    définition de fillable pour Produit

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'designation_produit',
+        'description_produit',
+        'prix',
+        'categorie_id',
+        'disponibilite',
+        'image',
+    ];    
 }
