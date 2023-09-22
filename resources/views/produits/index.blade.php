673ccb8aa1a962f98ee55624f1eab796da4a9b55
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-22 06:55:56 +0200

    afficher le message de réusitte

--- resources/views/produits/index.blade.php
+++ resources/views/produits/index.blade.php
@@ -18,6 +18,20 @@
         @endif
         <!-- Fin message d'erreur -->
 
+        <!-- Afficher le message d'enregistrement réussit -->
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <div class="alert-icon">
+                    <i class="far fa-fw fa-bell"></i>
+                </div>
+                <div class="alert-message">
+                    <strong>{{ $message }}</strong>
+                </div>
+            </div>
+        @endif
+        <!-- Fin message de réussite -->
+
         <!-- Header -->
         <div class="row mb-2 mb-xl-3">
             <div class="col-auto d-none d-sm-block">
