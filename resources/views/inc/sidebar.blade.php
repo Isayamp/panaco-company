16227b208964a98988c649c7cf78d6820abbd070
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-09-23 05:29:20 +0200

    desactiver le men active

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -15,7 +15,7 @@
         </a>
 
         <ul class="sidebar-nav">
-            <li class="sidebar-item active">
+            <li class="sidebar-item">
                 <a class="sidebar-link" href="{{ url('/dashboard') }}">
                     <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboards</span>
                 </a>
